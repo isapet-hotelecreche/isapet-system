7c4a81518cc571a5d7655d59037427f999752efe
isapet-hotelecreche <isapet.petsitter@gmail.com>
2025-11-26 10:21:37 -0300

    Add files via upload
    atualização alinhamento v1

--- app.js
+++ app.js
@@ -2266,7 +2266,7 @@ async function buildResumoPeriodo(fromISO, toISO){
             <col style="width:1%;">   <!-- ID -->
             <col style="width:14%;">  <!-- Período -->
             <col style="width:17%;">  <!-- Tutor & Pets -->
-			<col style="width:8%;">  <!-- Dias por Semana -->
+			<col style="width:3%;">  <!-- Dias por Semana -->
             <col style="width:12%;">  <!-- Valor -->
             <col style="width:12%;">  <!-- Status -->
             <col style="width:10%;">  <!-- Pagamento -->
